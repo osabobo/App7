101b48b0f3fd1650d4c3c7601337e073cec4250e
osagie eboigbe <osagie_eboigbe@yahoo.com>
2020-12-05 11:56:56 +0100

    file to add

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"your@domain.com\"\n\
+email = \"johanneseboigbe55@yahoo.com.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
