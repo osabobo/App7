9034082f254eba1202ad01c4fb0f5f9f06743472
osagie eboigbe <osagie_eboigbe@yahoo.com>
2020-12-05 12:26:54 +0100

    file added

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"johanneseboigbe55@yahoo.com.com\"\n\
-" > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
---
+++ uber_pickups.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+def main():
+    st.title('Uber pickups in NYC')
+
+
+
+if __name__ == '__main__':
+    main()
